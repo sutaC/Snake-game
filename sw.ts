0b5f49180ceb17846e96cab12b964df0928687f2
Catus <catus.pl@gmail.com>
2024-01-05 12:17:53 +0100

    New Icons and PWA colors adjustment

--- sw.ts
+++ sw.ts
@@ -2,115 +2,117 @@ const stataicCacheName = "site-static";
 const dynamicCacheName = "site-dynamic";
 const fallbackName = "./docs/pages/fallback.html";
 const assets = [
-	fallbackName,
-	"./",
-	"./index.html",
-	"./lib/style/style.css",
-	"https://fonts.googleapis.com/css2?family=Offside&display=swap",
-	"./lib/scripts/script.js",
-	"./lib/scripts/app.js",
-	"./lib/modules/swipe-detector.js",
-	"./lib/modules/snake-game.js",
-	"./docs/assets/illustration-win.png",
-	"./docs/assets/illustration-pause.png",
-	"./docs/assets/illustration-lose.png",
-	"./docs/assets/logo.png",
-	"./docs/assets/snake-head-down.svg",
-	"./docs/assets/snake-head-left.svg",
-	"./docs/assets/snake-head-right.svg",
-	"./docs/assets/snake-head-up.svg",
-	"./docs/assets/snake-tail.svg",
-	"./docs/assets/apple.svg",
-	"./docs/assets/icon-pause.svg",
-	"./docs/assets/icon-play.svg",
-	"./docs/assets/icon-reset.svg",
+    fallbackName,
+    "./",
+    "./index.html",
+    "./lib/style/style.css",
+    "https://fonts.googleapis.com/css2?family=Offside&display=swap",
+    "./lib/scripts/script.js",
+    "./lib/scripts/app.js",
+    "./lib/modules/swipe-detector.js",
+    "./lib/modules/snake-game.js",
+    "./docs/assets/illustration-win.png",
+    "./docs/assets/illustration-pause.png",
+    "./docs/assets/illustration-lose.png",
+    "./docs/assets/icon-48x48.png",
+    "./docs/assets/icon-128x128.png",
+    "./docs/assets/icon-512x512.png",
+    "./docs/assets/snake-head-down.svg",
+    "./docs/assets/snake-head-left.svg",
+    "./docs/assets/snake-head-right.svg",
+    "./docs/assets/snake-head-up.svg",
+    "./docs/assets/snake-tail.svg",
+    "./docs/assets/apple.svg",
+    "./docs/assets/icon-pause.svg",
+    "./docs/assets/icon-play.svg",
+    "./docs/assets/icon-reset.svg",
 ];
 
 async function cacheStatic(): Promise<void> {
-	const cache = await caches.open(stataicCacheName);
-	assets.forEach(async (asset) => {
-		try {
-			await cache.add(asset);
-		} catch (error) {
-			console.error(error);
-		}
-	});
+    const cache = await caches.open(stataicCacheName);
+    assets.forEach(async (asset) => {
+        try {
+            await cache.add(asset);
+        } catch (error) {
+            console.error(error);
+        }
+    });
 }
 
 async function deleteCache(): Promise<void> {
-	const keys = await caches.keys();
-	keys.forEach((key) => {
-		if (key != stataicCacheName) caches.delete(key);
-	});
+    const keys = await caches.keys();
+    keys.forEach((key) => {
+        if (key != stataicCacheName) caches.delete(key);
+    });
 }
 
 async function limitCacheSize(name: string, size: number): Promise<void> {
-	const cache = await caches.open(name);
-	const keys = await cache.keys();
+    const cache = await caches.open(name);
+    const keys = await cache.keys();
 
-	if (keys.length <= size || keys.length === 0) {
-		return;
-	}
+    if (keys.length <= size || keys.length === 0) {
+        return;
+    }
 
-	await cache.delete(keys[0]);
-	await limitCacheSize(name, size);
+    await cache.delete(keys[0]);
+    await limitCacheSize(name, size);
 }
 
 async function handleRespond(req: Request): Promise<Response | null> {
-	let cacheRes;
-	try {
-		cacheRes = (await caches.match(req)) as Response;
-	} catch (error) {
-		console.error(error);
-	}
+    let cacheRes;
+    try {
+        cacheRes = (await caches.match(req)) as Response;
+    } catch (error) {
+        console.error(error);
+    }
 
-	if (cacheRes) return cacheRes;
+    if (cacheRes) return cacheRes;
 
-	let fetchRes;
-	try {
-		fetchRes = await fetch(req);
-	} catch (error) {
-		console.error(error);
-	}
+    let fetchRes;
+    try {
+        fetchRes = await fetch(req);
+    } catch (error) {
+        console.error(error);
+    }
 
-	if (fetchRes) {
-		const cache = await caches.open(dynamicCacheName);
-		cache.put(req, fetchRes.clone());
-		await limitCacheSize(dynamicCacheName, 15);
-		return fetchRes;
-	}
+    if (fetchRes) {
+        const cache = await caches.open(dynamicCacheName);
+        cache.put(req, fetchRes.clone());
+        await limitCacheSize(dynamicCacheName, 15);
+        return fetchRes;
+    }
 
-	// Handle no cache & no connection
-	if (req.url.endsWith(".html")) {
-		const fallback = await caches.match(fallbackName);
-		if (fallback) return fallback;
-	}
+    // Handle no cache & no connection
+    if (req.url.endsWith(".html")) {
+        const fallback = await caches.match(fallbackName);
+        if (fallback) return fallback;
+    }
 
-	return null;
+    return null;
 }
 
 // ---
 
 self.addEventListener("install", (event: any) => {
-	try {
-		event.waitUntil(cacheStatic());
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        event.waitUntil(cacheStatic());
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 self.addEventListener("activate", (event: any) => {
-	try {
-		event.waitUntil(deleteCache());
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        event.waitUntil(deleteCache());
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 self.addEventListener("fetch", (event: any) => {
-	try {
-		event.respondWith(handleRespond(event.request));
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        event.respondWith(handleRespond(event.request));
+    } catch (error) {
+        console.error(error);
+    }
 });
